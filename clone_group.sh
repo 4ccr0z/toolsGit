3cbf59eadef892e171a9978bb241d49aca723479
zoccra <anthony.bourasseaumartin@gmail.com>
2023-09-14 20:49:25 +0200

    Update clone_group.sh

--- clone_group.sh
+++ clone_group.sh
@@ -7,5 +7,5 @@ URL=https://$DOMAIN
 JQ_PATH=/C/softs/jq
 for repo in $(curl -s --header "PRIVATE-TOKEN: $2" $URL/api/v4/groups/$1 | $JQ_PATH -r ".projects[].path_with_namespace"); do
   cd "$3";
-  git clone "https://$2@$DOMAIN$repo";
+  git clone "https://$2@$DOMAIN/$repo";
 done;
