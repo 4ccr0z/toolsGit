8c79811c18708b266ac1fcb7e9399fdb609e78f6
zoccra <anthony.bourasseaumartin@gmail.com>
2023-09-14 20:45:29 +0200

    Update clone_group.sh

--- clone_group.sh
+++ clone_group.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #1 $1 = group git
 #2 $2 = token gitlab
+#3 $3 = chemin du clone
 DOMAIN=gitlab.forgapp.com
 URL=https://$DOMAIN
 JQ_PATH=/C/softs/jq
 for repo in $(curl -s --header "PRIVATE-TOKEN: $2" $URL/api/v4/groups/$1 | $JQ_PATH -r ".projects[].path_with_namespace"); do
+  cd "$3";
   git clone "https://$2@$DOMAIN$repo";
 done;
